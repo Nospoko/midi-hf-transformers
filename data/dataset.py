f2596568b74c36a202d57d61a51f61d06cd19aa5
WojciechMat <wmatsad@gmail.com>
2023-10-13 17:34:16 +0200

    building dataset

--- data/dataset.py
+++ data/dataset.py
@@ -4,6 +4,7 @@ import json
 import hashlib
 
 import torch
+import pandas as pd
 import fortepyan as ff
 from tqdm import tqdm
 from omegaconf import OmegaConf, DictConfig
@@ -11,7 +12,77 @@ from torch.utils.data import Dataset as TorchDataset
 from datasets import Dataset, load_dataset, concatenate_datasets
 
 from data.tokenizer import MidiEncoder
-from data.quantizer import MidiQuantizer
+from data.quantizer import MidiQuantizer, MidiCTQuantizer
+
+
+def build_CT_translation_dataset(
+    dataset: Dataset,
+    dataset_cfg: DictConfig,
+) -> Dataset:
+    quantizer = MidiCTQuantizer(
+        n_duration_bins=dataset_cfg.quantization.duration,
+        n_velocity_bins=dataset_cfg.quantization.velocity,
+        n_start_bins=dataset_cfg.quantization.start,
+        sequence_duration=dataset_cfg.sequence_duration,
+    )
+
+    quantized_pieces = []
+
+    for it, record in tqdm(enumerate(dataset), total=len(dataset)):
+        piece = ff.MidiPiece.from_huggingface(record)
+        qpiece = quantizer.inject_quantization_features(piece)
+
+        # We want to get back to the original recording easily
+        qpiece.source |= {"base_record_id": it, "dataset_name": dataset.info.description}
+        quantized_pieces.append(qpiece)
+
+    chopped_sequences = []
+    for it, piece in tqdm(enumerate(quantized_pieces), total=len(quantized_pieces)):
+        chopped_sequences += CT_quantized_piece_to_records(
+            piece=piece,
+            sequence_duration=dataset_cfg.sequence_duration,
+            sequence_step=dataset_cfg.sequence_step,
+        )
+
+    new_dataset = Dataset.from_list(chopped_sequences)
+    return new_dataset
+
+
+def CT_quantized_piece_to_records(
+    piece: ff.MidiPiece,
+    sequence_duration: int,
+    sequence_step: int,
+):
+    chopped_sequences = []
+    n_samples = 1 + int(piece.df.iloc[-1]["start"] - sequence_duration) // sequence_step
+    df = piece.df
+    for jt in range(n_samples):
+        start = jt * sequence_step
+        finish = start
+
+        while df["start"][finish] - df["start"][start] < sequence_duration:
+            finish += 1
+
+        part = df.iloc[start:finish]
+
+        source = piece.source.copy()
+        source |= {"start": start, "finish": finish}
+
+        sequence = {
+            "pitch": part.pitch.astype("int16").values.T,
+            # Quantized features
+            "start_bin": part.start_bin.astype("int16").values.T,
+            "duration_bin": part.duration_bin.astype("int16").values.T,
+            "velocity_bin": part.velocity_bin.astype("int16").values.T,
+            # Ground truth
+            "start": part.start.values,
+            "end": part.end.values,
+            "duration": part.duration.values,
+            "velocity": part.velocity.values,
+            "source": json.dumps(source),
+        }
+        chopped_sequences.append(sequence)
+    return chopped_sequences
 
 
 def build_translation_dataset(
@@ -155,6 +226,34 @@ def shard_and_build(
     return processed_dataset
 
 
+def shard_and_build_CT(
+    dataset: Dataset,
+    dataset_cfg: DictConfig,
+    dataset_cache_path: str,
+    num_shards: int = 2,
+) -> Dataset:
+    shard_paths = []
+
+    for it in range(num_shards):
+        path = f"{dataset_cache_path}-part-{it}"
+
+        dataset_shard = dataset.shard(num_shards=num_shards, index=it)
+        print(f"Processing shard {it} of {num_shards} with {len(dataset_shard)} records.")
+
+        processed_shard = build_CT_translation_dataset(dataset_shard, dataset_cfg=dataset_cfg)
+        processed_shard.save_to_disk(path)
+        shard_paths.append(path)
+
+    processed_dataset = concatenate_datasets([Dataset.load_from_disk(path) for path in shard_paths])
+
+    for path in shard_paths:
+        for file in glob.glob(f"{path}/*"):
+            os.remove(file)
+        os.rmdir(path)
+
+    return processed_dataset
+
+
 def load_cache_dataset(
     dataset_cfg: DictConfig,
     dataset_name: str,
@@ -184,14 +283,70 @@ def load_cache_dataset(
 
     # hardcoded maximum shard size as 5000
     num_shards = len(midi_dataset) // 5000 + 1
-    translation_dataset = shard_and_build(
-        dataset=midi_dataset,
-        dataset_cfg=dataset_cfg,
-        num_shards=num_shards,
-        dataset_cache_path=dataset_cache_path,
-    )
+    if "dstart" in dataset_cfg.quantization:
+        translation_dataset = shard_and_build(
+            dataset=midi_dataset,
+            dataset_cfg=dataset_cfg,
+            num_shards=num_shards,
+            dataset_cache_path=dataset_cache_path,
+        )
+    else:
+        translation_dataset = shard_and_build_CT(
+            dataset=midi_dataset,
+            dataset_cfg=dataset_cfg,
+            num_shards=num_shards,
+            dataset_cache_path=dataset_cache_path,
+        )
     translation_dataset.save_to_disk(dataset_cache_path)
     # load dataset again to update cache_files attribute
     translation_dataset = Dataset.load_from_disk(dataset_cache_path)
 
     return translation_dataset
+
+
+def main():
+    dataset_name = "roszcz/maestro-v1-sustain"
+    dataset_cfg = {
+        "sequence_duration": 20,
+        "sequence_step": 10,
+        "quantization": {
+            "duration": 3,
+            "velocity": 3,
+            # 650 start bins sound nice :)
+            "start": 20,
+        },
+    }
+    cfg = OmegaConf.create(dataset_cfg)
+    dataset = load_cache_dataset(cfg, dataset_name, split="test")
+
+    quantizer = MidiCTQuantizer(
+        n_duration_bins=cfg.quantization.duration,
+        n_velocity_bins=cfg.quantization.velocity,
+        n_start_bins=cfg.quantization.start,
+        sequence_duration=cfg.sequence_duration,
+    )
+    record = pd.DataFrame(dataset[0])
+    record = quantizer.apply_quantization(record)
+    piece = ff.MidiPiece(record)
+    print(piece.df)
+    # ff.view.make_piano_roll_video(piece, "test.mp4")
+
+    from data.tokenizer import MultiTokEncoder, VelocityEncoder
+
+    keys = ["pitch", "start_bin", "duration_bin", "velocity_bin"]
+    src_encoder = MultiTokEncoder(cfg.quantization, keys=keys)
+    tgt_encoder = VelocityEncoder()
+    test_dataset = MyTokenizedMidiDataset(
+        dataset=dataset,
+        dataset_cfg=cfg,
+        src_encoder=src_encoder,
+        tgt_encoder=tgt_encoder,
+    )
+    print(test_dataset[0])
+
+    tokens = [src_encoder.vocab[idx] for idx in test_dataset[0]["source_token_ids"]]
+    print("src tokens: ", tokens)
+
+
+if __name__ == "__main__":
+    main()
