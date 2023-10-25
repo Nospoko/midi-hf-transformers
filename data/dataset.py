fixed average dist, training for single token

data/dataset.py
@@ -11,6 +11,8 @@ from matplotlib import pyplot as plt
 from omegaconf import OmegaConf, DictConfig
 from torch.utils.data import Dataset as TorchDataset
 from datasets import Dataset, load_dataset, concatenate_datasets
+
+from data.midiencoder import MidiEncoder
 from data.quantizer import MidiQuantizer, MidiATQuantizer
 from data.multitokencoder import MultiTokEncoder
 from data.maskedmidiencoder import MaskedMidiEncoder
@@ -160,7 +162,7 @@ class MyTokenizedMidiDataset(TorchDataset):
         self,
         dataset: Dataset,
         dataset_cfg: DictConfig,
-        encoder: MultiTokEncoder,
+        encoder: MultiTokEncoder | MidiEncoder,
     ):
         self.dataset = dataset
         self.dataset_cfg = dataset_cfg