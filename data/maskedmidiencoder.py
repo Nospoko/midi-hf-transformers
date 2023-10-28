fix bug in masking

data/maskedmidiencoder.py
@@ -22,7 +22,7 @@ class MaskedMidiEncoder:
     def vocab_size(self) -> int:
         return len(self.vocab)
 
-    def mask_record(self, record: dict) -> tuple[list[str], list[str]]:
+    def mask_record(self, record: dict) -> tuple[np.ndarray, np.ndarray]:
         """
         Mask record and return tuple of src and tgt tokens with masks.
         """
@@ -31,7 +31,7 @@ class MaskedMidiEncoder:
         num_masks = self.masking_probability * len(src_tokens)
 
         ids_to_mask = np.random.randint(len(src_tokens), size=int(num_masks))
-
+        print(ids_to_mask)
         np_src = np.array(src_tokens)
         np_tgt = np.array(tgt_tokens)
 
@@ -42,7 +42,7 @@ class MaskedMidiEncoder:
         np_src[ids_to_mask] = "<MASK>"
         np_tgt[tgt_mask] = "<MASK>"
 
-        return src_tokens, tgt_tokens
+        return np_src, np_tgt
 
     def encode_record(self, record: dict) -> tuple[list[int], list[int]]:
         """