ed912ee1c1f711ec651d413d670e3f5dca0445d6
WojciechMat <wmatsad@gmail.com>
2023-11-11 15:13:13 +0100

    remove prints

--- pipelines/T5/main.py
+++ pipelines/T5/main.py
@@ -132,8 +132,6 @@ def create_masked_datasets(
         base_encoder=base_encoder,
         encoder=encoder,
     )
-    print(len(train_dataset.encoder.vocab))
-    print(vocab_size(cfg))
     
     val_dataset = MaskedMidiDataset(
         dataset=val_translation_dataset,
@@ -160,7 +158,6 @@ def create_datasets_finetune(
     # use the same token ids as used during pre-training
     tokenizer.vocab = pretraining_tokenizer.vocab
     tokenizer.token_to_id = pretraining_tokenizer.token_to_id
-    print(len(tokenizer.vocab))
     train_dataset = MyTokenizedMidiDataset(
         dataset=train_translation_dataset,
         dataset_cfg=cfg.dataset,
