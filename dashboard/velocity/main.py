9fcb65e0ca1988a815329c2c52c0ead513411aed
WojciechMat <wmatsad@gmail.com>
2023-11-15 14:23:37 +0100

    update dashboard

--- dashboard/velocity/main.py
+++ dashboard/velocity/main.py
@@ -5,14 +5,16 @@ import json
 import torch
 import numpy as np
 import pandas as pd
-import torch.nn as nn
+import fortepyan as ff
+
 import streamlit as st
 from fortepyan import MidiPiece
 from omegaconf import OmegaConf, DictConfig
+from streamlit_pianoroll import from_fortepyan
 from transformers import T5Config, T5ForConditionalGeneration
 
 from data.midiencoder import VelocityEncoder
-from utils import vocab_size, piece_av_files
+from utils import vocab_size
 from data.maskedmidiencoder import MaskedMidiEncoder
 from data.dataset import MyTokenizedMidiDataset, load_cache_dataset
 from data.multitokencoder import MultiMidiEncoder, MultiVelocityEncoder
@@ -36,7 +38,7 @@ def main():
     with st.sidebar:
         # Show available checkpoints
         options = glob.glob("checkpoints/velocity/*.pt")
-        options.sort()
+        options.sort(reverse=True)
         checkpoint_path = st.selectbox(label="model", options=options)
         st.markdown("Selected checkpoint:")
         st.markdown(checkpoint_path)
@@ -52,6 +54,37 @@ def main():
     dataset_params = OmegaConf.to_container(train_cfg.dataset)
     st.markdown("Dataset config:")
     st.json(dataset_params, expanded=True)
+
+    # Folder to render audio and video
+    model_dir = f"tmp/dashboard/{train_cfg.run_name}"
+
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+
+    if mode == "Sequence predictions":
+        model_predictions_review(
+            checkpoint=checkpoint,
+            train_cfg=train_cfg,
+        )
+
+
+def model_predictions_review(
+    checkpoint: dict,
+    train_cfg: DictConfig,
+):
+    # load checkpoint, force dashboard device
+    dataset_cfg = train_cfg.dataset
+    dataset_name = st.text_input(label="dataset", value=train_cfg.dataset_name)
+    split = st.text_input(label="split", value="test")
+
+    random_seed = st.selectbox(label="random seed", options=range(20))
+
+    # load translation dataset and create MyTokenizedMidiDataset
+    val_translation_dataset = load_cache_dataset(
+        dataset_cfg=dataset_cfg,
+        dataset_name=dataset_name,
+        split=split,
+    )
 
     if "finetune" in train_cfg.train and train_cfg.train.finetune:
         tokenizer = MultiMidiEncoder(
@@ -75,14 +108,47 @@ def main():
             quantization_cfg=train_cfg.dataset.quantization,
             time_quantization_method=train_cfg.time_quantization_method,
         )
-    cls_token_id = tokenizer.token_to_id["<CLS>"]
-    pad_token_id = tokenizer.token_to_id["<PAD>"]
+
+    dataset = MyTokenizedMidiDataset(
+        dataset=val_translation_dataset,
+        dataset_cfg=train_cfg.dataset,
+        encoder=tokenizer,
+    )
+    start_token_id: int = dataset.encoder.token_to_id["<CLS>"]
+    pad_token_id: int = dataset.encoder.token_to_id["<PAD>"]
     config = T5Config(
         vocab_size=vocab_size(train_cfg),
-        decoder_start_token_id=cls_token_id,
-        eos_token_id=pad_token_id,
+        decoder_start_token_id=start_token_id,
         pad_token_id=pad_token_id,
+        eos_token_id=pad_token_id,
         use_cache=False,
+        d_model=train_cfg.model.d_model,
+        d_kv=train_cfg.model.d_kv,
+        d_ff=train_cfg.model.d_ff,
+        num_layers=train_cfg.model.num_layers,
+        num_heads=train_cfg.model.num_heads,
+    )
+
+    model = T5ForConditionalGeneration(config)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.eval().to(DEVICE)
+
+    n_parameters: float = sum(p.numel() for p in model.parameters()) / 1e6
+    st.markdown(f"Model parameters: {n_parameters:.3f}M")
+
+    start_token_id: int = dataset.encoder.token_to_id["<CLS>"]
+    pad_token_id: int = dataset.encoder.token_to_id["<PAD>"]
+    config = T5Config(
+        vocab_size=vocab_size(train_cfg),
+        decoder_start_token_id=start_token_id,
+        pad_token_id=pad_token_id,
+        eos_token_id=pad_token_id,
+        use_cache=False,
+        d_model=train_cfg.model.d_model,
+        d_kv=train_cfg.model.d_kv,
+        d_ff=train_cfg.model.d_ff,
+        num_layers=train_cfg.model.num_layers,
+        num_heads=train_cfg.model.num_heads,
     )
 
     model = T5ForConditionalGeneration(config)
@@ -91,47 +157,6 @@ def main():
 
     n_parameters = sum(p.numel() for p in model.parameters()) / 1e6
     st.markdown(f"Model parameters: {n_parameters:.3f}M")
-
-    # Folder to render audio and video
-    model_dir = f"tmp/dashboard/{train_cfg.run_name}"
-
-    if not os.path.exists(model_dir):
-        os.mkdir(model_dir)
-
-    if mode == "Sequence predictions":
-        model_predictions_review(
-            model=model,
-            train_cfg=train_cfg,
-            model_dir=model_dir,
-            tokenizer=tokenizer,
-        )
-
-
-def model_predictions_review(
-    model: nn.Module,
-    train_cfg: DictConfig,
-    model_dir: str,
-    tokenizer: MultiVelocityEncoder | MultiMidiEncoder | VelocityEncoder,
-):
-    # load checkpoint, force dashboard device
-    dataset_cfg = train_cfg.dataset
-    dataset_name = st.text_input(label="dataset", value=train_cfg.dataset_name)
-    split = st.text_input(label="split", value="test")
-
-    random_seed = st.selectbox(label="random seed", options=range(20))
-
-    # load translation dataset and create MyTokenizedMidiDataset
-    val_translation_dataset = load_cache_dataset(
-        dataset_cfg=dataset_cfg,
-        dataset_name=dataset_name,
-        split=split,
-    )
-
-    dataset = MyTokenizedMidiDataset(
-        dataset=val_translation_dataset,
-        dataset_cfg=train_cfg.dataset,
-        encoder=tokenizer,
-    )
 
     n_samples = 5
     np.random.seed(random_seed)
@@ -157,7 +182,7 @@ def model_predictions_review(
             max_length = len(src_token_ids)
         generated_velocity = model.generate(src_token_ids.unsqueeze(0), max_length=max_length)
         generated_velocity = generated_velocity.squeeze(0)
-        decoded_velocity = tokenizer.decode_tgt(generated_velocity)
+        generated_velocity = tokenizer.decode_tgt(generated_velocity)
 
         # TODO start here
         # Reconstruct the sequence as recorded
@@ -169,7 +194,7 @@ def model_predictions_review(
         pred_piece_df = true_piece.df.copy()
 
         # change untokenized velocities to model predictions
-        pred_piece_df["velocity"] = decoded_velocity
+        pred_piece_df["velocity"] = generated_velocity
         pred_piece_df["velocity"] = pred_piece_df["velocity"].fillna(0)
 
         # create quantized piece with predicted velocities
@@ -177,34 +202,20 @@ def model_predictions_review(
 
         pred_piece.source = true_piece.source.copy()
 
-        # create files
-        true_save_base = os.path.join(model_dir, f"true_{record_id}")
-        true_piece_paths = piece_av_files(piece=true_piece, save_base=true_save_base)
-
-        predicted_save_base = os.path.join(model_dir, f"predicted_{record_id}")
-        predicted_paths = piece_av_files(piece=pred_piece, save_base=predicted_save_base)
-
         # create a dashboard
         st.json(record_source)
         cols = st.columns(2)
-        source_tokens: list[str] = [dataset.encoder.vocab[idx] for idx in src_token_ids]
-        tgt_tokens: list[str] = [dataset.encoder.vocab[idx] for idx in record["target_token_ids"]]
-        generated_tokens: list[str] = [dataset.encoder.vocab[idx] for idx in generated_velocity]
         with cols[0]:
             # Unchanged
-            st.image(true_piece_paths["pianoroll_path"])
-            st.audio(true_piece_paths["mp3_path"])
-            st.markdown("**Source tokens:**")
-            st.markdown(source_tokens)
-            st.markdown("**Target tokens:**")
-            st.markdown(tgt_tokens)
+            fig = ff.view.draw_pianoroll_with_velocities(true_piece)
+            st.pyplot(fig)
+            from_fortepyan(true_piece)
 
         with cols[1]:
             # Predicted
-            st.image(predicted_paths["pianoroll_path"])
-            st.audio(predicted_paths["mp3_path"])
-            st.markdown("**Predicted tokens:**")
-            st.markdown(generated_tokens)
+            fig = ff.view.draw_pianoroll_with_velocities(pred_piece)
+            st.pyplot(fig)
+            from_fortepyan(pred_piece)
 
 
 if __name__ == "__main__":
